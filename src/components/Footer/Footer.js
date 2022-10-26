f12c6138ab903e0f67a91a1765b0f088894f84ab
faisalgit1 <mdfaisalahmed520@gmail.com>
2022-10-26 23:52:50 +0600

    footer-add

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,8 +2,35 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className='text-center'>
-
+        <div >
+            <footer className="px-4 py-8 dark:bg-gray-800 dark:text-gray-400">
+                <div className="container flex flex-wrap items-center justify-center mx-auto space-y-4 sm:justify-between sm:space-y-0">
+                    <div className="flex flex-row pr-3 space-x-4 sm:space-x-8">
+                        <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 rounded-full dark:bg-violet-400">
+                            <img src="https://kansasteachingandleadingproject.org/wp-content/uploads/2020/09/southeast-education-service-center-greenbush-continuous-learning-favicon.png" />
+                        </div>
+                        <ul className="flex flex-wrap items-center space-x-4 sm:space-x-8">
+                            <li>
+                                <a rel="noopener noreferrer" href="#">Terms of Use</a>
+                            </li>
+                            <li>
+                                <a rel="noopener noreferrer" href="#">Privacy</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <ul className="flex flex-wrap pl-3 space-x-4 sm:space-x-8">
+                        <li>
+                            <a rel="noopener noreferrer" href="#">Instagram</a>
+                        </li>
+                        <li>
+                            <a rel="noopener noreferrer" href="#">Facebook</a>
+                        </li>
+                        <li>
+                            <a rel="noopener noreferrer" href="#">Twitter</a>
+                        </li>
+                    </ul>
+                </div>
+            </footer>
         </div>
     );
 };
