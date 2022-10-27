79a4d1637c419bd68734d0a397db2e0b0c4089eb
faisalgit1 <mdfaisalahmed520@gmail.com>
2022-10-27 17:16:19 +0600

    loader-and-log-out-done

--- src/Context/AuthProvider.js
+++ src/Context/AuthProvider.js
@@ -1,6 +1,6 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 import app from '../firebase/firebase-config'
-import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth'
+import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth'
 
 export const AuthContext = createContext()
 
@@ -8,28 +8,43 @@ export const AuthContext = createContext()
 const AuthProvider = ({ children }) => {
 
     const auth = getAuth(app)
+
+    // User Data
+    const [user, setuser] = useState(null)
+
+    // Reload 
+    const [loading, setLoading] = useState(true)
+
     // Providers
     const googleProvider = new GoogleAuthProvider();
 
     // Google Sign in
     const googleSignIn = () => {
-
+        setLoading(true)
         return signInWithPopup(auth, googleProvider)
 
     }
 
     //Email Sign up 
     const signUp = (email, password) => {
-
+        setLoading(true)
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
     // Email Sign In
     const signIn = (email, password) => {
+        setLoading(false)
         return signInWithEmailAndPassword(auth, email, password)
     }
+
+    // Log out
+    const logOut = () => {
+        setLoading(true)
+        return signOut(auth)
+    }
+
     const authInfo = {
-        googleSignIn, signUp, signIn
+        user, signUp, signIn, googleSignIn,
     }
     return (
         <div>
